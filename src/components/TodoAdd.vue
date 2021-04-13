9aebdc2c339b89d8c16358b5cc595d540e56d3e8
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-04-13 18:58:46 +0700

    62130500127 [UPDATE] add post method im todo add

--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="todoAdd" class="grid grid-cols-8 gap-4">
-      <input type="text" class="col-span-8 border border-black px-2 pt-0.5 rounded-md text-xs">
-      <input type="date" class="col-span-4 text-xs">
-      <button class="w-full h-full bg-green-400 text-xs col-start-7 col-span-2 hover:bg-green-500">Submit</button>
+      <input type="text" class="col-span-8 border border-black px-2 pt-0.5 rounded-md text-xs" v-model="title">
+      <input type="date" class="col-span-4 text-xs" v-model="date">
+      <button class="w-full h-full bg-green-400 text-xs col-start-7 col-span-2 hover:bg-green-500" @click="submit">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoAdd"
+  name: "TodoAdd",
+  data() {
+    return {
+      title: null,
+      date: null
+    }
+  },
+  methods: {
+    submit(){
+      this.$emit("submit", this.title, this.date);
+    }
+  },
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-40">
-    <todo-add></todo-add>
+    <todo-add @submit="post"></todo-add>
     <todo-list class="mt-8"></todo-list>
   </div>
 </template>
@@ -8,8 +8,19 @@
 <script>
 import TodoAdd from "@/components/TodoAdd.vue";
 import TodoList from "@/components/TodoList.vue";
+import axios from "axios";
 export default {
   name: 'Home',
+  methods: {
+    post(title, date){
+      axios.post(" http://localhost:3000/todo", {
+        title: title,
+        date: date
+      }).then(()=>{
+        console.log("post succesful");
+      })
+    }
+  },
   components: {
     TodoAdd,
     TodoList

ea71a24d0c62b8150bacd26a4f9405d3ad9b1cc9
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-04-13 21:23:54 +0700

    62130500127 [FIX] todoAdd no filter

--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -4,6 +4,11 @@
       <input type="date" class="col-span-4 text-xs" v-model="date">
       <button class="w-full h-full bg-green-400 text-xs col-start-7 col-span-2 hover:bg-green-500" @click="submit">Submit</button>
   </div>
+  <ul class="text-red-500 font-light text-xxs">
+    <li v-for="(error, index) in errors" :key="index">
+      {{ error }}
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -12,11 +17,15 @@ export default {
   data() {
     return {
       title: null,
-      date: null
+      date: null,
+      errors: []
     }
   },
   methods: {
     submit(){
+      if (this.title == null || this.date == null) {
+        this.errors.push("Please fill the information");
+      }
       this.$emit("submit", this.title, this.date);
     }
   },

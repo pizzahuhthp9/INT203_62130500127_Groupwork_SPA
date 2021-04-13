bf67c49d0ff2b701f0ad8d30ccaff121fd442a88
pizzahuhthp9 <57580794+pizzahuhthp9@users.noreply.github.com>
2021-04-13 18:25:02 +0700

    62130500127 [FIX] Retext Size

--- src/components/TodoAdd.vue
+++ src/components/TodoAdd.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="todoAdd" class="grid grid-cols-8 gap-4">
-      <input type="text" class="col-span-8 border border-black px-2 pt-0.5 rounded-md text-sm">
-      <input type="date" class="col-span-4">
+      <input type="text" class="col-span-8 border border-black px-2 pt-0.5 rounded-md text-xs">
+      <input type="date" class="col-span-4 text-xs">
       <button class="w-full h-full bg-green-400 text-xs col-start-7 col-span-2 hover:bg-green-500">Submit</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: "TodoAdd"
 }
 </script>
 
